115392002706afe01e49913f717efb79ddc4a5e7
Juliano Sill <sill.juliano@gmail.com>
2023-11-04 21:49:25 -0300

    style(Button): update component style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,11 @@
-import { Container } from './styles'
+import PropTypes from 'prop-types'
+
+import * as S from './styles'
 
 export function Button({ children, ...rest }) {
-  return <Container {...rest}>{children}</Container>
+  return <S.Container {...rest}>{children}</S.Container>
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
 }
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -5,14 +5,17 @@ export const Container = styled.button`
   ${fontStyles.poppins100Medium}
   padding: 1.2rem 2.4rem;
 
+  color: ${({ theme }) => theme.COLORS.LIGHT_100};
   background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
   border: none;
   border-radius: 0.5rem;
-  color: ${({ theme }) => theme.COLORS.LIGHT_100};
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
   gap: 0.8rem;
 
-  width: ${props => props.$width};
+  transition: all 0.3s ease;
 
   &:hover {
     background-color: ${({ theme }) => theme.COLORS.TOMATO_200};
