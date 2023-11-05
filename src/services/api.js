c04a997a126dcacd708bafe0d36418b6ec7c5019
Juliano Sill <sill.juliano@gmail.com>
2023-11-05 11:31:02 -0300

    build(axios): setup api connection config

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const URL = 'http://localhost'
+const PORT = '3333'
+
+export const api = axios.create({
+  baseURL: `${URL}:${PORT}`,
+})
+
+export const imageBaseUrl = `${URL}:${PORT}/files`
