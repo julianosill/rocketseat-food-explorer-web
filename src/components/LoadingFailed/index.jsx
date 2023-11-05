56198b3ba80add183aab5cabb25333f33c1bc1e1
Juliano Sill <sill.juliano@gmail.com>
2023-11-05 16:11:34 -0300

    chore(LoadingFiled): update propTypes and import message handler

--- src/components/LoadingFailed/index.jsx
+++ src/components/LoadingFailed/index.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types'
 
 import { TbFaceIdError } from 'react-icons/tb'
+
+import { handleFailedMessage } from '../../services/handlers'
+
 import * as S from './styles'
 
-export function LoadingFailed({ text }) {
+export function LoadingFailed({ error }) {
   return (
     <S.Container>
       <TbFaceIdError />
-      <p>{text}</p>
+      <p>{handleFailedMessage(error.message)}</p>
     </S.Container>
   )
 }
 
 LoadingFailed.propTypes = {
-  text: PropTypes.string.isRequired,
+  error: PropTypes.object.isRequired,
 }
