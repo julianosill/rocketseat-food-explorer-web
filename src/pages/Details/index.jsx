4dc79477ea7187d427e7d0030d1ba30a83898e2b
Juliano Sill <sill.juliano@gmail.com>
2023-11-05 16:10:04 -0300

    feat(Details): create Details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,86 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { api } from '../../services/api'
 
 import { HiOutlineChevronLeft } from 'react-icons/hi'
-import { PiReceipt } from 'react-icons/pi'
 
-import { Wrapper } from '../../components/Wrapper'
-import { Main } from '../../components/Main'
-import { Header } from '../../components/Header'
-import { Footer } from '../../components/Footer'
-import { Stepper } from '../../components/Stepper'
-import { Button } from '../../components/Button'
+import { Loading } from '../../components/Loading'
+import { LoadingFailed } from '../../components/LoadingFailed'
+import { ProductDetailsCard } from '../../components/ProductDetailsCard'
+
 import * as S from './styles'
 
-const product = {
-  image: 'https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg',
-  name: 'Salada Ravanello',
-  description:
-    'Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.',
-  ingredients: ['alface', 'cebola', 'pão naan', 'pepino'],
-  price: 25.25,
-}
-
-function priceFormatter(amount) {
-  const price = amount.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  })
-  return price
-}
-
 export function Details() {
-  const [amount, setAmount] = useState(1)
-  const totalPrice = priceFormatter(amount * product.price)
+  const { id } = useParams()
+  const [product, setProduct] = useState(null)
+  const [isLoadingProduct, setIsLoadingProduct] = useState(false)
+  const [requestFailed, setRequestFailed] = useState(null)
 
-  function handleIncrease() {
-    setAmount(prev => prev + 1)
-  }
+  const navigate = useNavigate()
 
-  function handleDecrease() {
-    if (amount > 1) {
-      setAmount(prev => prev - 1)
+  useEffect(() => {
+    async function getProduct() {
+      setIsLoadingProduct(true)
+      await api
+        .get(`/products/${id}`)
+        .then(response => {
+          setProduct(response.data)
+          setRequestFailed(null)
+        })
+        .catch(error => {
+          console.log(error)
+          if (error.response.data) {
+            setRequestFailed(error.response.data)
+          }
+          setProduct(null)
+        })
+        .finally(() => setIsLoadingProduct(false))
     }
-  }
+    getProduct()
+  }, [id])
 
   return (
-    <>
-      <Wrapper>
-        <Header />
-        <Main>
-          <S.Details>
-            <S.Breadcrumbs>
-              <HiOutlineChevronLeft /> voltar
-            </S.Breadcrumbs>
-            <S.Content>
-              <img src={product.image} alt={product.name} />
-              <S.Infos>
-                <h1>{product.name}</h1>
-                <p>{product.description}</p>
-                {product.ingredients && (
-                  <S.Tags>
-                    {product.ingredients.map(tag => (
-                      <li key={tag}>
-                        <button>{tag}</button>
-                      </li>
-                    ))}
-                  </S.Tags>
-                )}
-                <S.Actions>
-                  <Stepper
-                    amount={amount}
-                    increase={handleIncrease}
-                    decrease={handleDecrease}
-                  />
-                  <Button>
-                    <PiReceipt /> incluir ∙ {totalPrice}
-                  </Button>
-                </S.Actions>
-              </S.Infos>
-            </S.Content>
-          </S.Details>
-        </Main>
-        <Footer />
-      </Wrapper>
-    </>
+    <S.Container>
+      <S.BackButton onClick={() => navigate(-1)}>
+        <HiOutlineChevronLeft /> voltar
+      </S.BackButton>
+      {isLoadingProduct && (
+        <Loading text="Carregando deliciosas informações..." />
+      )}
+      {product && <ProductDetailsCard product={product} />}
+      {requestFailed && <LoadingFailed error={requestFailed} />}
+    </S.Container>
   )
 }
