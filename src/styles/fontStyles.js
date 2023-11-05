8af3dc43c6eef6ebc7d516c395d9351cab164525
Juliano Sill <sill.juliano@gmail.com>
2023-11-05 11:29:55 -0300

    build(fontStyles): separate fontStyles from global

---
+++ src/styles/fontStyles.js
@@ -0,0 +1,105 @@
+const FONTS = {
+  poppinsRegular: `
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-weight: 400;
+  `,
+  poppinsMedium: `
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-weight: 500;
+  `,
+  poppinsBold: `
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+    font-weight: 700;
+  `,
+  robotoRegular: `
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-weight: 400;
+  `,
+  robotoBold: `
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-weight: 700;
+  `,
+}
+
+export const FONT_STYLES = {
+  poppins100Medium: `
+    ${FONTS.poppinsMedium}
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+  `,
+  poppins200Medium: `
+    ${FONTS.poppinsMedium}
+    font-size: 2rem;
+    line-height: 1.6;
+  `,
+  poppins300Regular: `
+    ${FONTS.poppinsRegular}
+    font-size: 2.4rem;
+    line-height: 1.4;
+  `,
+  poppins300Medium: `
+    ${FONTS.poppinsMedium}
+    font-size: 2.4rem;
+    line-height: 1.4;
+  `,
+  poppins300Bold: `
+    ${FONTS.poppinsBold}
+    font-size: 2.4rem;
+    line-height: 1.4;
+  `,
+  poppins400Medium: `
+    ${FONTS.poppinsMedium}
+    font-size: 3.2rem;
+    line-height: 1.4;
+  `,
+  poppins500Medium: `
+    ${FONTS.poppinsMedium}
+    font-size: 4rem;
+    line-height: 1.4;
+  `,
+  robotoSmallestRegular: `
+    ${FONTS.robotoRegular}
+    font-size: 1.2rem;
+    line-height: 1.6;
+  `,
+  robotoSmallerRegular: `
+    ${FONTS.robotoRegular}
+    font-size: 1.4rem;
+    line-height: 1.6;
+  `,
+  robotoSmallerBold: `
+    ${FONTS.robotoBold}
+    font-size: 1.4rem;
+    line-height: 1.6;
+  `,
+  robotoSmallSpaced: `
+    ${FONTS.robotoRegular}
+    font-size: 1.6rem;
+    line-height: 1.6;
+  `,
+  robotoSmallRegular: `
+    ${FONTS.robotoRegular}
+    font-size: 1.6rem;
+    line-height: 1;
+  `,
+  robotoBigBold: `
+    ${FONTS.robotoBold}
+    font-size: 2rem;
+    line-height: 1.6;
+  `,
+  robotoBiggerBold: `
+    ${FONTS.robotoBold}
+    font-size: 2.4rem;
+    line-height: normal;
+  `,
+  robotoBiggestRegular: `
+    ${FONTS.robotoRegular}
+    font-size: 3.2rem;
+    line-height: 1.6;
+  `,
+  robotoGiantBold: `
+    ${FONTS.robotoBold}
+    font-size: 4.2rem;
+    line-height: normal;
+  `,
+}
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,110 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-
-const fontWeight = {
-  poppinsRegular: `
-    font-family: 'Poppins', Arial, Helvetica, sans-serif;
-    font-weight: 400;
-  `,
-  poppinsMedium: `
-    font-family: 'Poppins', Arial, Helvetica, sans-serif;
-    font-weight: 500;
-  `,
-  poppinsBold: `
-    font-family: 'Poppins', Arial, Helvetica, sans-serif;
-    font-weight: 700;
-  `,
-  robotoRegular: `
-    font-family: 'Roboto', Arial, Helvetica, sans-serif;
-    font-weight: 400;
-  `,
-  robotoBold: `
-    font-family: 'Roboto', Arial, Helvetica, sans-serif;
-    font-weight: 700;
-  `,
-}
-
-export const fontStyles = {
-  poppins100Medium: `
-    ${fontWeight.poppinsMedium}
-    font-size: 1.4rem;
-    line-height: 2.4rem;
-  `,
-  poppins200Medium: `
-    ${fontWeight.poppinsMedium}
-    font-size: 2rem;
-    line-height: 1.6;
-  `,
-  poppins300Regular: `
-    ${fontWeight.poppinsRegular}
-    font-size: 2.4rem;
-    line-height: 1.4;
-  `,
-  poppins300Medium: `
-    ${fontWeight.poppinsMedium}
-    font-size: 2.4rem;
-    line-height: 1.4;
-  `,
-  poppins300Bold: `
-    ${fontWeight.poppinsBold}
-    font-size: 2.4rem;
-    line-height: 1.4;
-  `,
-  poppins400Medium: `
-    ${fontWeight.poppinsMedium}
-    font-size: 3.2rem;
-    line-height: 1.4;
-  `,
-  poppins500Medium: `
-    ${fontWeight.poppinsMedium}
-    font-size: 4rem;
-    line-height: 1.4;
-  `,
-  robotoSmallestRegular: `
-    ${fontWeight.robotoRegular}
-    font-size: 1.2rem;
-    line-height: 1.6;
-  `,
-  robotoSmallerRegular: `
-    ${fontWeight.robotoRegular}
-    font-size: 1.4rem;
-    line-height: 1.6;
-  `,
-  robotoSmallerBold: `
-    ${fontWeight.robotoBold}
-    font-size: 1.4rem;
-    line-height: 1.6;
-  `,
-  robotoSmallSpaced: `
-    ${fontWeight.robotoRegular}
-    font-size: 1.6rem;
-    line-height: 1.6;
-  `,
-  robotoSmallRegular: `
-    ${fontWeight.robotoRegular}
-    font-size: 1.6rem;
-    line-height: 1;
-  `,
-  robotoBigBold: `
-    ${fontWeight.robotoBold}
-    font-size: 2rem;
-    line-height: 1.6;
-  `,
-  robotoBiggerBold: `
-    ${fontWeight.robotoBold}
-    font-size: 2.4rem;
-    line-height: normal;
-  `,
-  robotoBiggestRegular: `
-    ${fontWeight.robotoRegular}
-    font-size: 3.2rem;
-    line-height: 1.6;
-  `,
-  robotoGiantBold: `
-    ${fontWeight.robotoBold}
-    font-size: 4.2rem;
-    line-height: normal;
-  `,
-}
+import { FONT_STYLES } from './fontStyles'
 
 export default createGlobalStyle`
 
@@ -141,45 +36,21 @@ ul {
   list-style: none;
 }
 
-input {
-  ${fontStyles.robotoSmallRegular}
-
-  &::placeholder {
-    color: ${({ theme }) => theme.COLORS.LIGHT_500};
-  }
-
-  &:focus {
-    border-color: ${({ theme }) => theme.COLORS.CAKE_200};
-    outline-color: ${({ theme }) => theme.COLORS.CAKE_200};
-    outline-width: 1px;
-    outline-style: solid;
-  }
-}
-
 label {
-  ${fontStyles.robotoSmallRegular}
-  
+  ${FONT_STYLES.robotoSmallRegular}
   display: block;
   cursor: pointer;
   color: ${({ theme }) => theme.COLORS.LIGHT_400};
-
 }
 
-button {
-  ${fontStyles.poppins100Medium}
-  
+button {  
   background: none;
   border: none;
   cursor: pointer;
   outline-offset: 4px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  transition: all 0.3s ease;
   
   &:focus {
+    border-radius: 0.5rem;
     outline-color: ${({ theme }) => theme.COLORS.CAKE_200};
     outline-width: 1px;
     outline-style: solid;
@@ -189,6 +60,18 @@ button {
     cursor: default;
     outline: none;
   }
-}
 
+  /* keyframes animation */
+
+  @keyframes spin {
+    from { transform: rotate(0deg) }
+    to { transform: rotate(360deg) }
+  }
+
+  @keyframes pulse {
+    0%, 100% { opacity: 1 }
+    50% { opacity: 0.5 }
+  }
+
+}
 `
