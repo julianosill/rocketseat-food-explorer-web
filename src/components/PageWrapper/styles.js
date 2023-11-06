b2c519c2279a5ed41a6ccd3b6ccdcc232e1a6644
Juliano Sill <sill.juliano@gmail.com>
2023-11-05 22:49:43 -0300

    style(PageWrapper): updar Main margin-bottom

--- src/components/PageWrapper/styles.js
+++ src/components/PageWrapper/styles.js
@@ -18,7 +18,7 @@ export const Main = styled.main`
   width: 100%;
   max-width: ${BREAKPOINTS.XL};
   margin: 0 auto;
-  padding: 12.5rem 12.3rem 2.8rem;
+  padding: 12.5rem 12.3rem 4.8rem;
 
   @media (max-width: ${BREAKPOINTS.MD}) {
     padding: 12.5rem 2.8rem 2.8rem;
