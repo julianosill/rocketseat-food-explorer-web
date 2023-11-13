ea9436815cf5ea985cd79108c1438caf238d9b24
Juliano Sill <sill.juliano@gmail.com>
2023-11-13 17:00:20 -0300

    refactor(UploadInput): add Enter function and focus style

--- src/components/admin/UploadInput/index.jsx
+++ src/components/admin/UploadInput/index.jsx
@@ -1,19 +1,38 @@
 import PropTypes from 'prop-types'
 
+import { useRef } from 'react'
 import { FiUpload } from 'react-icons/fi'
+
+import { Label } from '../../Label'
 
 import * as S from './styles'
 
-export function UploadInput({ label, image, ...props }) {
-  return (
-    <S.Container $image={image}>
-      <label htmlFor={props.id}>
-        {image ? <img src={image} alt="Foto do produto" /> : <FiUpload />}
+export function UploadInput({ label, text, image, ...props }) {
+  const labelRef = useRef()
+  const inputRef = useRef()
 
-        {label}
-      </label>
-      <input type="file" {...props} />
-    </S.Container>
+  return (
+    <>
+      {label && <Label htmlFor={props.id}>{label}</Label>}
+      <S.Container $image={image}>
+        <label ref={labelRef} htmlFor={props.id}>
+          {image ? <img src={image} alt="Foto do produto" /> : <FiUpload />}
+          {text}
+        </label>
+        <input
+          type="file"
+          ref={inputRef}
+          onFocus={() => labelRef.current.classList.add('has-focus')}
+          onBlur={() => labelRef.current.classList.remove('has-focus')}
+          onKeyDownCapture={e => {
+            if (e.code === 'Enter') {
+              inputRef.current.click()
+            }
+          }}
+          {...props}
+        />
+      </S.Container>
+    </>
   )
 }
 
--- src/components/admin/UploadInput/styles.js
+++ src/components/admin/UploadInput/styles.js
@@ -2,22 +2,30 @@ import styled from 'styled-components'
 import { FONT_STYLES } from '../../../styles/fontStyles'
 
 export const Container = styled.div`
+  position: relative;
   width: 100%;
 
-  background-color: ${({ theme }) => theme.COLORS.DARK_800};
-  border-radius: 0.8rem;
-
-  > label {
+  label {
     ${FONT_STYLES.poppins100Medium}
 
     padding: ${({ $image }) => ($image ? '0.8rem 3.2rem' : '1.2rem 3.2rem')};
 
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    background-color: ${({ theme }) => theme.COLORS.DARK_800};
+    border-radius: 0.8rem;
+
     cursor: pointer;
 
     display: flex;
     align-items: center;
     gap: 0.8rem;
+
+    &.has-focus {
+      border-color: ${({ theme }) => theme.COLORS.CAKE_200};
+      outline-color: ${({ theme }) => theme.COLORS.CAKE_200};
+      outline-width: 1px;
+      outline-style: solid;
+    }
 
     > img {
       height: 3.2rem;
@@ -28,8 +36,13 @@ export const Container = styled.div`
     }
   }
 
-  > input {
+  input {
     position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
     opacity: 0;
   }
 `
