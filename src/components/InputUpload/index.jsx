c1e622263433441f6b8ca7dfe38ea5550aa485f2
Juliano Sill <sill.juliano@gmail.com>
2023-11-17 23:05:00 -0300

    feat(InputUpload): create component

---
+++ src/components/InputUpload/index.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types'
+
+import { useRef } from 'react'
+import { FiUpload } from 'react-icons/fi'
+
+import { Label } from '../Label'
+import * as S from './styles'
+
+export function InputUpload({ label, text, image, ...props }) {
+  const labelRef = useRef()
+  const inputRef = useRef()
+
+  return (
+    <>
+      {label && <Label htmlFor={props.id}>{label}</Label>}
+      <S.Container $image={image}>
+        <label ref={labelRef} htmlFor={props.id}>
+          {image ? <img src={image} alt="Foto do produto" /> : <FiUpload />}
+          {text}
+        </label>
+        <input
+          type="file"
+          ref={inputRef}
+          onFocus={() => labelRef.current.classList.add('has-focus')}
+          onBlur={() => labelRef.current.classList.remove('has-focus')}
+          {...props}
+        />
+      </S.Container>
+    </>
+  )
+}
+
+InputUpload.propTypes = {
+  label: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  id: PropTypes.string.isRequired,
+}
