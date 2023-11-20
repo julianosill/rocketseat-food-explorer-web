dd30a4763996cf29a4fb2c24b448119707a33da9
Juliano Sill <sill.juliano@gmail.com>
2023-11-20 17:42:15 -0300

    feat(Input): include label sr-only

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types'
 import { Label } from '../Label'
 import * as S from './styles'
 
-export function Input({ label, icon: Icon, ...props }) {
+export function Input({ label, labelSrOnly, icon: Icon, ...props }) {
   return (
     <>
+      {labelSrOnly && (
+        <label htmlFor={props.id} className="sr-only">
+          {labelSrOnly}
+        </label>
+      )}
       {label && <Label htmlFor={props.id}>{label}</Label>}
       <S.Container>
         {Icon && (
@@ -21,6 +26,7 @@ export function Input({ label, icon: Icon, ...props }) {
 
 Input.propTypes = {
   label: PropTypes.string,
+  labelSrOnly: PropTypes.string,
   icon: PropTypes.func,
   id: PropTypes.string,
 }
