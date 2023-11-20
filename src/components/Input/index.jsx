5938c987c45e25f3d82bdd721659125f2ca421ca
Juliano Sill <sill.juliano@gmail.com>
2023-11-20 17:07:08 -0300

    feat(Input): add Icon to component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,5 +1,26 @@
-import { Container } from './styles'
+import PropTypes from 'prop-types'
 
-export function Input({ ...props }) {
-  return <Container {...props} />
+import { Label } from '../Label'
+import * as S from './styles'
+
+export function Input({ label, icon: Icon, ...props }) {
+  return (
+    <>
+      {label && <Label htmlFor={props.id}>{label}</Label>}
+      <S.Container>
+        {Icon && (
+          <S.Icon htmlFor={props.id}>
+            <Icon />
+          </S.Icon>
+        )}
+        <S.Input $hasIcon={Icon} {...props} />
+      </S.Container>
+    </>
+  )
+}
+
+Input.propTypes = {
+  label: PropTypes.string,
+  icon: PropTypes.func,
+  id: PropTypes.string,
 }
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,11 +1,47 @@
 import styled from 'styled-components'
+import { FONT_STYLES } from '../../styles/fontStyles'
 
-export const Container = styled.input`
-  background-color: ${({ theme }) => theme.COLORS.DARK_900};
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+export const Icon = styled.label`
+  position: absolute;
+
+  height: 100%;
+  padding: 0 1.4rem;
+  font-size: 2.4rem;
+
   color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+`
+
+export const Input = styled.input`
+  ${FONT_STYLES.robotoSmallRegular}
+
+  width: 100%;
+  height: fit-content;
+  padding: ${({ $hasIcon }) =>
+    $hasIcon ? '1.2rem 1.4rem 1.2rem 4.8rem' : '1.2rem 1.4rem'};
+  line-height: 1.5;
+
+  color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  background-color: ${({ theme }) => theme.COLORS.DARK_800};
   border: none;
   border-radius: 0.8rem;
 
-  padding: 1.2rem 1.4rem;
-  width: 100%;
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.LIGHT_500};
+  }
+
+  &:focus {
+    border-color: ${({ theme }) => theme.COLORS.CAKE_200};
+    outline-color: ${({ theme }) => theme.COLORS.CAKE_200};
+    outline-width: 1px;
+    outline-style: solid;
+  }
 `
