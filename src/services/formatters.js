88602ed60fe7820c38d9fe1cc031476934894ef1
Juliano Sill <sill.juliano@gmail.com>
2023-11-20 21:43:30 -0300

    chore: move and remove unused components

--- src/services/formatters.js
+++
@@ -1,7 +0,0 @@
-export function priceFormatter(amount) {
-  const price = amount.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  })
-  return price
-}
--- src/services/handlers.js
+++
@@ -1,8 +0,0 @@
-export function handleFailedMessage(message) {
-  switch (message) {
-    case 'product/product-not-found':
-      return 'Produto não encontrado.'
-    default:
-      return 'Ops, ocorreu algum erro.'
-  }
-}
