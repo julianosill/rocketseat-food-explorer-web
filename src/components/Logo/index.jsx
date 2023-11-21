6eeb483c1a4b762d23ada7c85ba1e70375d563d4
Juliano Sill <sill.juliano@gmail.com>
2023-11-21 18:36:24 -0300

    refactor(Logo): use html instead of svg

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,26 +1,30 @@
 import PropTypes from 'prop-types'
 
-import logo from '../../assets/logos/logo.svg'
-import logoAdmin from '../../assets/logos/logoAdmin.svg'
-import logoFooter from '../../assets/logos/logoFooter.svg'
+import * as S from './styles'
 
-import { Container } from './styles'
-
-export function Logo({ admin, footer }) {
-  const altText = 'Food Explorer'
-
-  if (footer) {
-    return <Container src={logoFooter} alt={altText} />
-  }
-
-  if (admin) {
-    return <Container src={logoAdmin} alt={altText} />
-  }
-
-  return <Container src={logo} alt={altText} />
+export function Logo({ admin, header, footer }) {
+  return (
+    <S.Container $header={header} $footer={footer}>
+      <svg
+        width="26"
+        height="30"
+        viewBox="0 0 26 30"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13.0001 0L25.9905 7.5V22.5L13.0001 30L0.00976562 22.5V7.5L13.0001 0Z"
+          fill="currentColor"
+        />
+      </svg>
+      <div>food explorer</div>
+      {admin && <span>admin</span>}
+    </S.Container>
+  )
 }
 
 Logo.propTypes = {
   admin: PropTypes.bool,
+  header: PropTypes.bool,
   footer: PropTypes.bool,
 }
--- src/components/Logo/styles.js
+++ src/components/Logo/styles.js
@@ -1,6 +1,55 @@
 import styled from 'styled-components'
+import { FONT_STYLES } from '../../styles/fontStyles'
+import { BREAKPOINTS } from '../../styles/breakpoints'
 
-export const Container = styled.img`
-  width: 100%;
-  height: auto;
+export const Container = styled.div`
+  position: relative;
+
+  color: ${({ theme, $footer }) =>
+    $footer ? theme.COLORS.LIGHT_700 : theme.COLORS.CAKE_100};
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  svg {
+    width: 2.6rem;
+    height: auto;
+  }
+
+  div {
+    ${FONT_STYLES.robotoBiggerBold}
+
+    color: ${({ theme, $footer }) =>
+      $footer ? theme.COLORS.LIGHT_700 : theme.COLORS.LIGHT_100};
+  }
+
+  span {
+    ${FONT_STYLES.robotoSmallestRegular}
+
+    position: absolute;
+    right: 0;
+    bottom: -1.2rem;
+
+    color: ${({ theme }) => theme.COLORS.CAKE_200};
+  }
+
+  @media (max-width: ${BREAKPOINTS.SM}) {
+    gap: ${({ $header }) => ($header ? '0.8rem' : '0.6rem')};
+
+    svg {
+      width: ${({ $header }) => ($header ? '2.4rem' : '1.8rem')};
+    }
+
+    div {
+      font-size: ${({ $header }) => ($header ? '2.1rem' : '1.5rem')};
+    }
+  }
+
+  @media (min-width: ${BREAKPOINTS.MIN_XS}) and (max-width: ${BREAKPOINTS.SM}) {
+    span {
+      position: ${({ $header }) => ($header ? 'relative' : null)};
+      bottom: ${({ $header }) => ($header ? '-0.1rem' : null)};
+    }
+  }
 `
