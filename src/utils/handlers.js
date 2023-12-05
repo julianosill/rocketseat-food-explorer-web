f440017810e6321a8749a140ea2bf20417c593cf
Juliano Sill <sill.juliano@gmail.com>
2023-12-05 10:30:55 -0300

    feat(handleFailedMessage): create handler function

--- src/utils/handlers.js
+++ src/utils/handlers.js
@@ -1,6 +1,22 @@
+import { ERROR_MESSAGES } from './errorMessages'
+
 export function handleFilterByCategory(category, productList) {
   const filteredProducts = productList.filter(
     product => product.category === category
   )
   return filteredProducts
+}
+
+export function handleFailedMessage(category, message) {
+  if (!message) {
+    return ERROR_MESSAGES['default']
+  }
+
+  if (category && message) {
+    for (const category of Object.keys(ERROR_MESSAGES)) {
+      const errorMessage =
+        ERROR_MESSAGES[category][message] || ERROR_MESSAGES['default']
+      return errorMessage
+    }
+  }
 }
