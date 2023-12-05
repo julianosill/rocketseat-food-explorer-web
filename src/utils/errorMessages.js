3ab137af3c3f7572176a135069d5b565be2b3f8e
Juliano Sill <sill.juliano@gmail.com>
2023-12-05 10:45:26 -0300

    feat(errorMessages): create error messages list

---
+++ src/utils/errorMessages.js
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+  auth: {
+    'auth/email-already-exists': 'E-mail já está registrado',
+    'auth/email-or-password-incorrect': 'E-mail e/ou senha incorretos',
+    'auth/invalid-email': 'E-mail inválido',
+    'auth/password-too-short': 'Senha é muito curta',
+  },
+  default: 'Algo deu errado. Por favor, contate nosso suporte.',
+}
