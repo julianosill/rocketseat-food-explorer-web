616d432c95580018dbd2af7a18d82f09c7bf7254
Juliano Sill <sill.juliano@gmail.com>
2023-12-15 22:24:33 -0300

    style(PageWrapper): keep header on top and scroll content

--- src/components/PageWrapper/index.jsx
+++ src/components/PageWrapper/index.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types'
-
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 import { Header } from '../Header'
 import { Container } from '../Container'
 import { Footer } from '../Footer'
-
 import * as S from './styles'
 
 export function PageWrapper({ page }) {
   return (
-    <S.Container>
+    <S.Wrapper>
       <Header />
-      <S.Main>
-        <Container>{page}</Container>
-      </S.Main>
-      <Footer />
+      <S.Content>
+        <S.Main>
+          <Container>{page}</Container>
+        </S.Main>
+        <Footer />
+      </S.Content>
       <ToastContainer
         theme="dark"
-        autoClose={3000}
-        style={{ fontSize: '1.6rem' }}
+        autoClose={2500}
+        style={{ fontSize: '1.6rem', lineHeight: '1.25' }}
       />
-    </S.Container>
+    </S.Wrapper>
   )
 }
 
--- src/components/PageWrapper/styles.js
+++ src/components/PageWrapper/styles.js
@@ -1,28 +1,30 @@
 import styled from 'styled-components'
 import { BREAKPOINTS } from '../../styles/breakpoints'
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   width: 100%;
-  min-height: 100vh;
-  min-height: 100svh;
+  height: 100vh;
+  height: 100svh;
 
   display: flex;
   flex-direction: column;
+`
 
-  > main {
-    flex: 1;
+export const Content = styled.div`
+  flex: 1;
+  width: 100%;
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.4rem;
+
+  @media (max-width: ${BREAKPOINTS.SM}) {
+    gap: 5.6rem;
   }
 `
 
 export const Main = styled.main`
   width: 100%;
-  padding: 12.5rem 0 4.8rem;
-
-  @media (max-width: ${BREAKPOINTS.MD}) {
-    padding: 12.5rem 0 4.8rem;
-  }
-
-  @media (max-width: ${BREAKPOINTS.SM}) {
-    padding: 13rem 0 4.8rem;
-  }
+  flex: 1;
 `
