e9fa3a71a4d2ec6932478a8196ec6ca7c873a1d4
Juliano Sill <sill.juliano@gmail.com>
2023-12-16 15:41:06 -0300

    chore(StateProvider): remove

--- src/hooks/components.useStateProvider.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-
-import { StateContext } from './components.StateProvider'
-
-export function useStateProvider() {
-  const context = useContext(StateContext)
-  return context
-}
