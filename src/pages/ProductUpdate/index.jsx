e13007d38f96092c05248d2409442dd3212590c3
Juliano Sill <sill.juliano@gmail.com>
2023-12-16 20:55:17 -0300

    chore(ProductUpdate): handle api error and update import component

--- src/pages/ProductUpdate/index.jsx
+++ src/pages/ProductUpdate/index.jsx
@@ -1,54 +1,71 @@
 import { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-
-import { HiOutlineChevronLeft } from 'react-icons/hi'
+import { useParams } from 'react-router-dom'
+import { MdOutlineNoFood } from 'react-icons/md'
 
 import { api } from '../../services/api'
+import { handleFailedMessage } from '../../utils/handlers'
 
+import { Loading } from '../../components/Loading'
+import { BackButton } from '../../components/BackButton'
 import { PageTitle } from '../../components/PageTitle'
 import { FormProduct } from '../../components/FormProduct'
-import { LoadingFailed } from '../../components/LoadingFailed'
+import { Empty } from '../../components/Empty'
 import * as S from './styles'
 
 export function ProductUpdate() {
   const { id } = useParams()
+  const [isLoading, setIsLoading] = useState(false)
   const [product, setProduct] = useState(null)
   const [error, setError] = useState('')
-  const navigate = useNavigate()
 
   useEffect(() => {
     async function getProduct() {
+      setIsLoading(true)
       await api
         .get(`/products/${id}`, { withCredentials: true })
         .then(response => {
           setProduct(response.data)
         })
         .catch(error => {
-          console.error(error)
+          let errorMessage
           if (error.response) {
-            setError(error.response.data.message)
+            errorMessage = error.response.data.message || null
           }
+          setError(handleFailedMessage('product', errorMessage))
         })
+        .finally(() => setIsLoading(false))
     }
     getProduct()
   }, [id])
 
-  return (
-    <>
-      <S.BackButton onClick={() => navigate(-1)}>
-        <HiOutlineChevronLeft /> voltar
-      </S.BackButton>
+  if (isLoading) {
+    return (
+      <S.LoadingContainer>
+        <Loading text="Carregando informações do produto" />
+      </S.LoadingContainer>
+    )
+  }
 
+  return (
+    <S.Container>
+      <BackButton size="sm" />
       {product ? (
         <>
           <S.Header>
-            <PageTitle text="Editar produto" />
+            <PageTitle>Editar produto</PageTitle>
           </S.Header>
           <FormProduct data={product} />
         </>
       ) : (
-        <LoadingFailed message={error} />
+        <S.EmptyContainer>
+          <Empty.Root orientation="vertical">
+            <Empty.Icon icon={MdOutlineNoFood} />
+            <Empty.Content>
+              <Empty.Title text={error} />
+            </Empty.Content>
+          </Empty.Root>
+        </S.EmptyContainer>
       )}
-    </>
+    </S.Container>
   )
 }
--- src/pages/ProductUpdate/styles.js
+++ src/pages/ProductUpdate/styles.js
@@ -1,29 +1,31 @@
 import styled from 'styled-components'
-import { FONT_STYLES } from '../../styles/fontStyles'
 import { BREAKPOINTS } from '../../styles/breakpoints'
 
-export const BackButton = styled.button`
-  ${FONT_STYLES.poppins300Bold}
-
-  color: ${({ theme }) => theme.COLORS.LIGHT_300};
-
-  display: flex;
-  align-items: center;
-
-  > svg {
-    font-size: 3.2rem;
-  }
+export const Container = styled.div`
+  height: 100%;
+  padding-top: 4rem;
 
   @media (max-width: ${BREAKPOINTS.SM}) {
-    ${FONT_STYLES.poppins300Medium}
-    font-size: 1.6rem;
-
-    > svg {
-      font-size: 2.2rem;
-    }
+    padding-top: 1rem;
   }
 `
 
 export const Header = styled.header`
   margin: 2.4rem 0 3.2rem;
+`
+
+export const LoadingContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+`
+
+export const EmptyContainer = styled.div`
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
