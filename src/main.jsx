fca8a40a5fd2c5df3ca2d1101b8ee66d5abebd34
Juliano Sill <sill.juliano@gmail.com>
2023-12-17 20:24:58 -0300

    chore(main): add providers and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { AuthProvider } from './hooks/auth.AuthProvider'
+import { CartProvider } from './hooks/cart.CartProvider'
+import { FavoriteProvider } from './hooks/favorite.favoriteProvider'
+import { Routes } from './routes'
+
 import theme from './styles/theme'
-import { Details } from './pages/Details'
+import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <AuthProvider>
+        <CartProvider>
+          <FavoriteProvider>
+            <Routes />
+          </FavoriteProvider>
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
